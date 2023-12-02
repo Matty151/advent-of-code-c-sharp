0e27b7e9302e129097f78a5403c2b66b6442f673
Matty <mattygr12345@hotmail.com>
2023-12-02 13:09:37 +0100

    Day 1 - Part 1.

--- Day1/Part1.cs
+++ Day1/Part1.cs
@@ -1,9 +1,29 @@
-﻿namespace AdventOfCode.Day1;
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Day1;
 
 public class Part1
 {
     public static void Run()
     {
-        Console.WriteLine("Hello World!");
+        Regex regex = new(@"\d");
+
+        List<int> calibrationValues = new();
+
+        foreach (string line in File.ReadLines(Path.Join(AppDomain.CurrentDomain.BaseDirectory, "../../../Day1/input/puzzle.txt"))) {
+            MatchCollection digits = regex.Matches(line);
+
+            if (digits.Count == 0) {
+                continue;
+            }
+
+            int number = int.Parse($"{digits[0]}{(digits.Count > 1 ? digits.Last() : digits[0])}");
+
+            calibrationValues.Add(number);
+        }
+
+        int total = calibrationValues.Sum();
+
+        Console.WriteLine(total);
     }
 }
